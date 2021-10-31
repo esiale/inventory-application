2b09983e109868380869989bfd4e10f91b669d11
esiale <blazejp0@gmail.com>
2021-10-31 15:40:46 +0100

    Add method to add products

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,6 +2,7 @@ const Product = require('../models/product');
 const Album = require('../models/album');
 const Format = require('../models/format');
 const mongoose = require('mongoose');
+const { body, validationResult } = require('express-validator');
 
 exports.product_list = (req, res, next) => {
   const count_products = Product.countDocuments({}).exec();
@@ -60,7 +61,61 @@ exports.product_create_get = (req, res, next) => {
     .catch((error) => next(error));
 };
 
-exports.product_create_post = (req, res, next) => {};
+exports.product_create_post = [
+  body('album').trim().notEmpty().withMessage('* Album is required').escape(),
+  body('price')
+    .trim()
+    .notEmpty()
+    .withMessage('* Price is required')
+    .isInt()
+    .withMessage('* Price must be a number')
+    .escape(),
+  body('number_in_stock')
+    .trim()
+    .notEmpty()
+    .withMessage('* Number in stock is required')
+    .isInt()
+    .withMessage('* Number in stock must be a number')
+    .escape(),
+  body('format').trim().notEmpty().withMessage('* Format is required').escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    const product = new Product({
+      album: req.body.album,
+      price: req.body.price,
+      number_in_stock: req.body.number_in_stock,
+      format: req.body.format,
+    });
+    if (!errors.isEmpty()) {
+      const fetch_albums = Album.find().exec();
+      const fetch_formats = Format.find().exec();
+      Promise.all([fetch_albums, fetch_formats]).then((results) => {
+        res.render('product_form', {
+          title: 'InventoryApp - add product',
+          albums: results[0],
+          formats: results[1],
+          product: product,
+          errors: errors.array({ onlyFirstError: true }),
+        });
+      });
+    } else {
+      Product.findOne({
+        album: req.body.album,
+        format: req.body.format,
+      }).exec((err, found_product) => {
+        if (err) return next(err);
+        if (found_product) {
+          res.redirect(found_product.url);
+        } else {
+          product.save((err) => {
+            if (err) return next(err);
+            res.redirect(product.url);
+          });
+        }
+      });
+    }
+  },
+];
 
 exports.product_delete_get = function (req, res) {
   res.send('NOT IMPLEMENTED');
